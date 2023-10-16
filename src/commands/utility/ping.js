17b901f5f4566c09c706347e02d8d4df978cb9c1
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-10-16 20:39:52 +0200

    ping command

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,13 +1,61 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("UTILITY » Replies with Pong!")
+    .setDescription(
+      "UTILITY » Get advanced information about Discord and Bot latency."
+    )
     .setDescriptionLocalizations({
-      "es-ES": "UTILITY » Responde con un Pong!",
+      "es-ES":
+        "UTILITY » Obtén información avanzada sobre la latencia de Discord y Bot.",
     }),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    let circles = {
+      good: ":white_check_mark: ",
+      okay: ":part_alternation_mark: ",
+      bad: ":x: ",
+    };
+
+    await interaction.deferReply();
+
+    const pinging = await interaction.editReply({ content: "Pinging..." });
+
+    const ws = interaction.client.ws.ping;
+    const msgEdit = Date.now() - pinging.createdTimestamp;
+
+    let days = Math.floor(interaction.client.uptime / 86400000);
+    let hours = Math.floor(interaction.client.uptime / 3600000) % 24;
+    let minutes = Math.floor(interaction.client.uptime / 60000) % 60;
+    let seconds = Math.floor(interaction.client.uptime / 1000) % 60;
+
+    const wsEmoji =
+      ws <= 100 ? circles.good : ws <= 200 ? circles.okay : circles.bad;
+    const msgEmoji = msgEdit <= 200 ? circles.good : circles.bad;
+
+    const pingEmbed = new EmbedBuilder()
+      .setThumbnail(interaction.client.user.displayAvatarURL({ size: 64 }))
+      .setFooter({
+        text: `Requested by ${interaction.user.tag}`,
+        iconURL: interaction.user.displayAvatarURL(),
+      })
+      .setColor("#5865F2")
+      .setTimestamp()
+      .addFields(
+        {
+          name: "Websocket Latency",
+          value: `${wsEmoji} \`${ws}ms\``,
+        },
+        {
+          name: "API Latency",
+          value: `${msgEmoji} \`${msgEdit}ms\``,
+        },
+        {
+          name: `Uptime`,
+          value: `⏰ \`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds\``,
+        }
+      );
+
+    await pinging.edit({ embeds: [pingEmbed], content: "\u200b" });
   },
 };

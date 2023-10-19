f653a401f98571b17848997195f43996239c13af
blaise.tk <iahispano0@gmail.com>
2023-10-19 08:57:28 +0200

    logs on start

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -1,14 +1,33 @@
-const { Events, ActivityType } = require("discord.js");
+const { Events, ActivityType, EmbedBuilder } = require("discord.js");
+const { logsChannelId } = require("../config.json");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
+ async execute(client) {
     require("../deployment/deployCommands.js");
     client.user.setPresence({
       activities: [{ name: "applio.org", type: ActivityType.Watching }],
       status: "online",
     });
+
+      try {
+        const channel = client.channels.cache.get(logsChannelId);
+
+        const embed = new EmbedBuilder()
+          .setColor("Green")
+          .setTimestamp()
+          .setDescription(
+            `✅`,
+          )
+
+        await channel.send({
+          embeds: [embed],
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
     console.log(`Starting the bot as ${client.user.tag}...`);
   },
 };

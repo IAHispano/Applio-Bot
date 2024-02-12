519d0f9e1183003d96eb52095c8098e1404c273d
Blaise <133521603+blaise-tk@users.noreply.github.com>
2024-02-12 15:02:43 +0100

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,19 +1,16 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMembers,
-  ],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 });
 module.exports = client;
 
 require("./deployment/registerCommands.js");
 require("./deployment/registerEvents.js");
 
-client.login(process.env.BOT_TOKEN).catch(() => {
+client.login(process.env.BOT_TOKEN).catch((error) => {
   console.log(
-    "[ERROR] Could not log into the bot, check your token and try again.",
+    "[ERROR] Could not log into the bot, check your token and try again." +
+      error
   );
 });

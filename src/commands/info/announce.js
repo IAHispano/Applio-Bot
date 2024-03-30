8b6402e391b3fbeee179e9780095d1e0156f5ef1
Blaise <blaise@iahispano.com>
2024-03-30 23:23:09 +0100

    try to fix command

--- src/commands/info/announce.js
+++ src/commands/info/announce.js
@@ -43,10 +43,7 @@ module.exports = {
       .getString("message")
       .replace(/\\n/g, "\n");
     const attachment = interaction.options.getAttachment("attachment") || null;
-    const channels = ["1188315275393777785", "1159380240271953940"];
-    const channelNames = channels.map(
-      (id) => client.channels.cache.get(id)?.name
-    );
+    const channels = ["1188315275393777785", "1206185734659252225"];
 
     const cancelButton = new ButtonBuilder()
       .setCustomId("cancelAnnouncement")
@@ -55,16 +52,25 @@ module.exports = {
 
     const row = new ActionRowBuilder().addComponents(cancelButton);
 
+    const channelOptions = channels.map((channelId) => {
+      return {
+        label: channelId,
+        value: channelId,
+      };
+    });
+
+    if (channelOptions.length === 0) {
+      interaction.reply({
+        content: "Error: Could not find any channels to send the message to.",
+        ephemeral: true,
+      });
+      return;
+    }
+
     const selectMenu = new StringSelectMenuBuilder()
       .setCustomId("selectedChannels")
       .setPlaceholder("Select channels to send the message")
-      .addOptions(
-        channelNames.map((name, index) => ({
-          label: name,
-          value: channels[index],
-          description: client.channels.cache.get(channels[index]).guild.name,
-        }))
-      )
+      .addOptions(channelOptions)
       .setMaxValues(channels.length)
       .setMinValues(1);
 
@@ -87,14 +93,25 @@ module.exports = {
       time: 30000, // Timeout after 30 seconds
     });
 
-    collector.on("collect", (i) => {
+    collector.on("collect", async (i) => {
       if (i.isStringSelectMenu()) {
         const selectedChannelIds = i.values;
-        const selectedChannels = selectedChannelIds.map((id) =>
-          client.channels.cache.get(id)
+        const selectedChannels = [];
+
+        // Using broadcastEval to search for channels across all shards
+        const res = await client.shard.broadcastEval(
+          (c, ids) => {
+            return ids.map((id) => c.channels.cache.get(id));
+          },
+          { context: selectedChannelIds }
         );
 
-        if (selectedChannels.some((channel) => !channel)) {
+        // Flattening the array of arrays into a single array of channels
+        for (const channels of res) {
+          selectedChannels.push(...channels.filter((channel) => channel));
+        }
+
+        if (selectedChannels.length !== selectedChannelIds.length) {
           i.update({
             content: ":x: One or more selected channels not found.",
             components: [],
@@ -102,6 +119,7 @@ module.exports = {
           return;
         }
 
+        // Sending message to selected channels
         for (const channel of selectedChannels) {
           channel.send({
             content: message.replace(/\\n/g, "\n"),
@@ -114,15 +132,30 @@ module.exports = {
           components: [],
         });
       } else if (i.customId === "confirmAnnouncement") {
-        for (const channelId of channels) {
-          const channel = client.channels.cache.get(channelId);
+        const allChannels = client.channels
+          .fetch({
+            cache: false,
+            force: true,
+          })
+          .then((channels) => channels.array());
+
+        const textChannels = allChannels.filter(
+          (channel) => channel.type === "GUILD_TEXT"
+        );
+        const channelsToSend = textChannels.filter((channel) =>
+          channels.includes(channel.id)
+        );
+
+        for (const channel of channelsToSend) {
           if (channel) {
             channel.send({
               content: message.replace(/\\n/g, "\n"),
               files: attachment ? [attachment] : [],
             });
           } else {
-            console.error(`Channel with ID ${channelId} not found.`);
+            console.error(
+              `Channel with ID ${channel.id} not found or is not a text-based channel.`
+            );
           }
         }
         i.update({

f8a497031da7c89400f5d0567009e23344980e15
Vidalnt <raddiscord1@gmail.com>
2024-05-31 15:26:40 -0500

    Fix MessageReference

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,4 +1,4 @@
-const { Events, EmbedBuilder } = require("discord.js");
+const { Events, EmbedBuilder, ButtonBuilder, ActionRowBuilder } = require("discord.js");
 
 const client = require("../bot.js");
 const prefix = `<@${process.env.BOT_ID}>`
@@ -10,19 +10,40 @@ module.exports = {
     let command
     if (message.type === 19 && message.reference) {
       const msg = await message.fetchReference()
+      message.applioReplied = message.content
+      message.applioReference = msg.content
       if (msg.author.id === process.env.BOT_ID) {
         new_ = `What you said before: ${msg.content} What I answer: ${message.content}`
         message.content = new_
         command = message.client.commands.get("chat");
       } else {
-        return
+        if (!message.content.startsWith(prefix)) return
+        message.content = message.content.slice(prefix.length).trim();
+        new_ = `Friend's question ${msg.author.username} said: "${msg.content}" (You can talk about ${msg.author.username} since it is a conversation between me and him and I want to share it with you.) I ask you with respect to the above (Me): "${message.content}" Your opinion on this matter:`;
+        message.content = new_
+        command = message.client.commands.get("chat");
       }
 
     } else {
       if (!message.content.startsWith(prefix)) return
       let args = message.content.toLowerCase().substring(prefix.length).split(" ");
       if (!args[1] || !args[1].trim()) {
+        let embed = new EmbedBuilder()
+        .setTitle("Applio Bot")
+        .setDescription("Applio is a Voice Conversion based on VITS focused on simplicity, quality and performance, if you need to know more about it, you can check [Website](https://applio.org) or [Github](https://github.com/IAHispano/Applio). \n Commands: \n - </search:1229146911483760781>\n- </searchuser:1232443891513561141>\n- </chat:1234277316994007040> \n*And More...*")
+        const invite = new ButtonBuilder()
+            .setStyle(5)
+            .setURL("https://discord.gg/IAHispano")
+            .setLabel("Discord Support")
+            .setEmoji("⚙️");
+        const inviteBot = new ButtonBuilder()
+            .setStyle(5)
+            .setURL("https://discord.com/api/oauth2/authorize?client_id=1144714449563955302&permissions=1376674433127&scope=bot")
+            .setLabel("Invite")
+            .setEmoji("🪛");
 
+        const row = new ActionRowBuilder().addComponents(invite, inviteBot);
+        await message.channel.send({ embeds: [embed], components: [row] })
         return
       }
       command = message.client.commands.get(args[1]);
@@ -41,7 +62,12 @@ module.exports = {
 
 
 async function executeCommand(command, message, channel) {
-  let content = message.content.slice(prefix.length).trim();
+  let content
+  if (message.content.includes(prefix)) {
+    content = message.content.slice(prefix.length).trim();
+  } else {
+    content = message.content
+  }
 
   message.options = {
     getString: () => content

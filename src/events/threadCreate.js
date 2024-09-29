15402e2fb6303921c00a916520471a01174c8170
Vidalnt <raddiscord1@gmail.com>
2024-09-29 17:43:35 -0500

    fix: :bug: Minor Things

--- src/events/threadCreate.js
+++ src/events/threadCreate.js
@@ -6,7 +6,8 @@ module.exports = {
 	async execute(thread) {
 		if (thread.parentId === process.env.AI_HISPANO_REQUEST_MODELS_CHANNEL_ID) {
 			try {
-				await thread.send(`Hey, your submission sounds interesting. But have you tried to check if the model already exists? If you haven't take a look at https://applio.org/models. If you find it feel free to close this thread.\n-# You can also run the </search:1170110962968301720> command to search for a model by name.`);
+				const threadOwner = await thread.fetchOwner(); 
+				await thread.send(`Hey <@${threadOwner.id}>, your submission sounds interesting. But have you tried to check if the model already exists? If you haven't take a look at https://applio.org/models. If you find it feel free to close this thread.\n-# You can also run the </search:1170110962968301720> command to search for a model by name.`);
 			} catch (error) {
 				console.error('Error sending thread creation message:', error);
 			}
